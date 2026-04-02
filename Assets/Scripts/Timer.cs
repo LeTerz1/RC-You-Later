7cb73825bb565e2128314ed0be184040eba985e7
LeTerz1 <trz.matthieu@gmail.com>
2026-04-02 11:22:27 +0200

    save + load

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using UnityEngine;
 
 public static class Timer
 {
     private static readonly Stopwatch stopwatch = new();
-    private static readonly List<long> steps = new(); //variable a save
+    private static readonly List<long> steps = new(); 
 
     public static bool IsRunning
     {
@@ -53,7 +55,17 @@ public static class Timer
     public static void Save()
     {
         // TODO : save our time steps (line 7 of this script) inside a file.
-        //test
+        string path = Application.dataPath + "/score.txt";
+
+        List<string> lines = new List<string>();
+
+        foreach (long step in steps)
+        {
+            lines.Add(step.ToString());
+        }
+
+        File.WriteAllLines(path, lines);
+        UnityEngine.Debug.Log("Saved score to " + path);
     }
 
     public static void Load()
@@ -61,5 +73,25 @@ public static class Timer
         // TODO : load our time steps from a file (if we have any)
         // and store them inside our steps variable (line 7 of this script)
         // to show them to the player before starting a race.
+
+        string path = Application.dataPath + "/score.txt";
+
+        if (!File.Exists(path))
+        {
+            UnityEngine.Debug.Log("pas de fichier score");
+            return;
+        }
+
+        string[] lines = File.ReadAllLines(path);
+        steps.Clear();
+        foreach (string line in lines)
+        {
+            if (long.TryParse(line, out long value))
+            {
+                steps.Add(value);
+            }
+        }
+
+        UnityEngine.Debug.Log("Loaded score from " + path);
     }
 }
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Timer.Reset();
+        Timer.Start();
+        Timer.Step();
+        Timer.Step();
+        Timer.Step();
+        Timer.Save();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
